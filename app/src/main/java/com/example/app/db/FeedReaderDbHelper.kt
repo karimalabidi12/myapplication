a40b61f30882820c62fd0c012155d763c2ad4dd9
karima <karimalabidi015@gmail.com>
2024-11-27 08:27:00 +0100

    Votre message

--- app/src/main/java/com/example/app/db/FeedReaderDbHelper.kt
+++ app/src/main/java/com/example/app/db/FeedReaderDbHelper.kt
@@ -1,6 +1,4 @@
 package com.example.app.db
-
-
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -12,8 +10,6 @@ class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         db.execSQL(SQL_CREATE_ENTRIES)
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        // This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
         db.execSQL(SQL_DELETE_ENTRIES)
         onCreate(db)
     }
@@ -21,7 +17,6 @@ class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         onUpgrade(db, oldVersion, newVersion)
     }
     companion object {
-        // If you change the database schema, you must increment the database version.
         const val DATABASE_VERSION = 1
         const val DATABASE_NAME = "FeedReader.db"
     }
